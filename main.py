60a6709e1aa94c1eec319c66673750223426e29c
Alec German <kimarimt@protonmail.com>
2024-11-01 14:29:56 -0400

    add count_characters function to main.py

--- main.py
+++ main.py
@@ -5,12 +5,27 @@ def count_word(text):
     return len(text.split())
 
 
+def count_characters(text):
+    lowercased = text.lower()
+    characters = {}
+
+    for ch in lowercased:
+        if ch in characters:
+            characters[ch] += 1
+        else:
+            characters[ch] = 1
+
+    return characters
+
+
 def main():
     with open(BOOK_PATH) as f:
         file_contents = f.read()
         words = count_word(file_contents)
+        characters = count_characters(file_contents)
 
         print(words)
+        print(characters)
 
 
 if __name__ == '__main__':
