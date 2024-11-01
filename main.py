2b47d76b221b86ce56143075c0408ec89268ca71
Alec German <kimarimt@protonmail.com>
2024-11-01 12:56:39 -0400

    added count_word function to main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 BOOK_PATH = 'books/frankenstein.txt'
 
 
+def count_word(text):
+    return len(text.split())
+
+
 def main():
     with open(BOOK_PATH) as f:
         file_contents = f.read()
+        words = count_word(file_contents)
 
-        print(file_contents)
+        print(words)
 
 
 if __name__ == '__main__':
